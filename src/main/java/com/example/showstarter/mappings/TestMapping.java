484499f56b0dd6640f6ac8531eb12c7decf51ad7
adamhusted <hustedaa@g.cofc.edu>
2024-02-21 20:54:20 -0500

    Created TestMapping.java

---
+++ src/main/java/com/example/showstarter/mappings/TestMapping.java
@@ -0,0 +1,14 @@
+package com.example.showstarter.mappings;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestMapping {
+
+	@GetMapping("/testing")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
