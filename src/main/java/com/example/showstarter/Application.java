e63f8d6255c47e5e8b7df87e00231e8e6fc43bec
adamhusted <hustedaa@g.cofc.edu>
2024-02-22 18:23:59 -0500

    Troubleshoot

--- src/main/java/com/example/showstarter/Application.java
+++ src/main/java/com/example/showstarter/Application.java
@@ -1,12 +1,7 @@
 package com.example.showstarter;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -21,11 +16,6 @@ public class Application {
     @GetMapping
     public String hello() {
         return "Hello World";
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
     }
     
 }
