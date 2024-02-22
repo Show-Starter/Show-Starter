0322b54311bfa03008c13601c78715ef6e4a5c6e
adamhusted <hustedaa@g.cofc.edu>
2024-02-22 18:01:48 -0500

    DB Connector

--- src/main/java/com/example/showstarter/Application.java
+++ src/main/java/com/example/showstarter/Application.java
@@ -1,7 +1,12 @@
 package com.example.showstarter;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,6 +21,11 @@ public class Application {
     @GetMapping
     public String hello() {
         return "Hello World";
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
     
 }
--- src/main/java/com/example/showstarter/mappings/Event.java
+++ src/main/java/com/example/showstarter/mappings/Event.java
@@ -1,19 +1,24 @@
 package com.example.showstarter.mappings;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.SQLException;
+// import java.sql.ResultSet;
+// import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Configuration
 public class Event {
-	int eventID;
+    int eventID;
 	String eventName;
 	String location;
 	String eventType;
 	String eventDate;
 	String eventTime;
 	int invoiceNum;
-	
 	
 	public Event(int eventID, String eventName, String location, String eventType, String eventDate, String eventTime, int invoiceNum) {
 		this.eventID = eventID;
@@ -24,54 +29,47 @@ public class Event {
 		this.eventTime = eventTime;
 		this.invoiceNum = invoiceNum;
 	}
+
+    public Event() {
+        //TODO Auto-generated constructor stub
+    }
+
+    @Autowired
+    public Event(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 	
 	// Pulls all events from database and creates an event based on each tuple in the events table
 	public static Event[] get_all_events() {
-		Event[] events = new Event[0];
-		int numEvents = 0;
-		
-		try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch (java.lang.ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-
-        String url = "jdbc:postgresql://lallah.db.elephantsql.com:5432/orunmzej";
-        String username = "orunmzej";
-        String password = "WkVYLIBK36XLSG5ceqh9oQuYTbEEndjH";
-
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
         try {
-            Connection db = DriverManager.getConnection(url, username, password);
-            Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM events");
-            while (rs.next()) {
-            	numEvents++;
-            }
-            events = new Event[numEvents];
-            rs = st.executeQuery("SELECT * FROM events");
-            int i = 0;
-            while (rs.next()) {
-                events[i] = new Event(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getInt(7));
-                i++;
-            }
-            rs.close();
-            st.close();
-            }
-        catch (java.sql.SQLException e) {
-            System.out.println(e.getMessage());
+            jdbcTemplate = DatabaseConfig.jdbcTemplate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-		
-		return events;
-	}
 
-    public static void main(String[] arg) {
+        String sql = "SELECT * FROM events";
+        List<Event> events = jdbcTemplate.query(sql, new CustomRowMapperEvent());
+
+        Event[] eventsArr = new Event[events.size()];
+
+        for (int i = 0; i < events.size(); i++) {
+            eventsArr[i] = events.get(i);
+        }
+
+        return eventsArr;
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of Event to access non-static methods
+        // JdbcTemplate jdbcTemplate = new JdbcTemplate(); // You may need to create this instance
         Event[] events = get_all_events();
 
         System.out.println(events.length);
-        // System.out.println(events[10].eventName);
     }
-	
-	
-	
+
 }
