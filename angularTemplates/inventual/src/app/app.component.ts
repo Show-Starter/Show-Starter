5b4f9fa51628fc46681c5773667f1477c3cdd591
ryanbaileh <55721920+ryanbaileh@users.noreply.github.com>
2024-02-22 22:41:36 -0500

    Changed Logo

--- angularTemplates/inventual/src/app/app.component.ts
+++ angularTemplates/inventual/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, Renderer2, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Show Starter';
+  title = 'inventual';
 
   isRTL = false;
 
