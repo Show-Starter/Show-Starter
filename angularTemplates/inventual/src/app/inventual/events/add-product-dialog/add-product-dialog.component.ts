401b027cfdd6f8ef3c31f68887b45ce68c3dda2e
ryanbaileh <55721920+ryanbaileh@users.noreply.github.com>
2024-04-15 22:39:51 -0400

    add product now has event types that you can choose from

--- angularTemplates/inventual/src/app/inventual/events/add-product-dialog/add-product-dialog.component.ts
+++ angularTemplates/inventual/src/app/inventual/events/add-product-dialog/add-product-dialog.component.ts
@@ -11,6 +11,12 @@ interface Task {
   completed: boolean;
   color: ThemePalette;
 }
+interface EventProductMap {
+  [key: string]: string[];
+  Birthday: string[];
+  Convention: string[];
+  Wedding: string[];
+}
 
 @Component({
   selector: 'app-add-product-dialog',
@@ -20,23 +26,71 @@ interface Task {
 export class AddProductDialogComponent implements OnInit {
 
   public products: Product[] = [];
-  
+
   public parsedProducts: Product[] = [];
   public searchText: string = '';
+  public selectedEventType: string = '';
   public tasks: Task[] = [];
   public currentPage: number = 1;
   public itemsPerPage: number = 10;
   public allComplete: boolean = false;
   public selectedProducts: Product[] = [];
+  public eventProductMap: EventProductMap = {
+    'Birthday': [
+      "Obey 40 Controller Power Supply",
+      "Microphone Stand",
+      "Digital Wireless Handheld Microphone System with SM58 Capsule",
+      "Quad Mic Kit",
+      "Twinkle Lights - Warm White LED 50'- White Wire",
+      "Twinkle Light Bar - LED - Warm White",
+      "Marquee Letter - H - 36",
+      "Marquee Letter - A - 36",
+      "Marquee Letter - P - 36",
+
+      "Marquee Letter - P - 36",
+      "Marquee Letter - Y - 36",
+      "Marquee Letter - B - 36",
+      "Marquee Letter - I - 36",
+      "Marquee Letter - T - 36",
+      "Marquee Letter - H - 36",
+      "Marquee Letter - D - 36",
+      "Marquee Letter - A - 36",
+      "Marquee Letter - Y - 36",
+
+      "DJ System 1"
+    ],
+    'Convention': [
+      "Obey 40 Controller Power Supply",
+      "Microphone Stand",
+      "Digital Wireless Handheld Microphone System with SM58 Capsule",
+      "Quad Mic Kit",
+      "DJ System 1",
+      "Stage Panel 4ftx8ft",
+      "Stage legs 24\""
+    ],
+    'Wedding': [
+      "Obey 40 Controller Power Supply",
+      "Microphone Stand",
+      "Digital Wireless Handheld Microphone System with SM58 Capsule",
+      "Quad Mic Kit",
+      "DJ System 1",
+      "Stage Panel 4ftx8ft",
+      "Stage legs 24\""
+    ]
+  };
+
 
   constructor(private productService: ProductService,
-              private MatDialog: MatDialog) { }
+    private MatDialog: MatDialog) {
+     
+    }
 
   ngOnInit(): void {
     this.getProducts();
     this.productService.getProducts().subscribe(products => {
       this.products = products;
     });
+    
   }
   public task: Task = {
     name: '',
@@ -54,9 +108,9 @@ export class AddProductDialogComponent implements OnInit {
       this.selectedProducts = this.selectedProducts.filter(p => p.id !== product.id);
     }
   }
-  
+
   public filterProducts(): void {
-   
+
     if (this.searchText == '') {
       this.initParse(); // If no search text, show all products
     } else {
@@ -68,7 +122,7 @@ export class AddProductDialogComponent implements OnInit {
       );
     }
   }
-  
+
   private refreshProducts(): void {
     this.getProducts(); // Re-fetch the products after deletion
   }
@@ -90,6 +144,7 @@ export class AddProductDialogComponent implements OnInit {
         }
         this.initParse();
         this.filterProducts();
+        
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
@@ -107,6 +162,32 @@ export class AddProductDialogComponent implements OnInit {
     }
   }
 
+  testFunction() {
+    console.log('Button clicked!');
+}
+  //For Curated Events button 
+
+  selectEventType(eventType: string) {
+    console.log('Event Type Selected:', eventType);
+    if (eventType in this.eventProductMap) {
+        this.selectedEventType = eventType;
+        this.filterProductsForEvent();
+    } else {
+        console.error('Invalid event type selected:', eventType);
+    }
+}
+  
+filterProductsForEvent() {
+  const eventTypeProducts = this.eventProductMap[this.selectedEventType];
+  if (!eventTypeProducts) {
+      console.error('No products array found for the selected event type:', this.selectedEventType);
+      this.parsedProducts = [];
+      return;
+  }
+  this.parsedProducts = this.products.filter(product => 
+    eventTypeProducts.some(ep => product.name.toLowerCase().includes(ep.toLowerCase()))
+);
+}
 
 
   public updateAllComplete(): void {
